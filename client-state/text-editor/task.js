9f7cee9050cf3e458bc59af2ec97649173b7e0bb
Galiullin Azat <galiullin.ar651@gmail.com>
2022-08-06 11:35:54 +0300

    внес поправки в текстовый редактор

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,17 +2,18 @@ const button = document.createElement('button');
 const textArea = document.getElementById('editor');
 const storage = localStorage.getItem('value');
 
-if (storage) {
-  textArea.value = storage;
-}
+// if (storage) {
+//   textArea.value = storage;
+// }
+
+textArea.value = storage;
 
 button.textContent = 'Очистить';
 button.style.borderRadius = '8px';
 button.style.background = '#0000ff'
 
 textArea.addEventListener('change', function (e) {
-  e.preventDefault();
-  console.log(e.target);
+  
   const storedValue = e.target.value;
   localStorage.setItem('value', storedValue);
 })
@@ -20,6 +21,6 @@ textArea.addEventListener('change', function (e) {
 document.querySelector('.card').appendChild(button);
 
 button.addEventListener('click', function (e) {
-  e.preventDefault();
   textArea.value = '';
+  localStorage.removeItem('value');
 })
